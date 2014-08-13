35356a77de7f254116a80089ac914109677bd22d
Florian Konnertz <f.konnertz@gmail.com>
2014-08-13 09:01:47 +0200

    feature dirs and extracting planner features

--- setup_planner.sh
+++ setup_planner.sh
@@ -4,3 +4,10 @@ cd $DRUPAL_ROOT/profiles/planner/modules/features
 
 git clone http://git.syscomp.de/syscomp_ldap_config
 
+cd $DRUPAL_ROOT/profiles/planner/modules/planner
+
+tar=~/git-test/planner/features/*.tar
+for i in `ls $tar`
+do
+    tar xf $i
+done
